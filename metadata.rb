2f655aa39d4a1b3a5d21d6c3a620c88f1df70656
ROMI <romi.afr@gmail.com>
2019-04-02 14:06:09 +0000

    first commit

---
+++ metadata.rb
@@ -0,0 +1,19 @@
+name 'belajar-chef-hactiv8-wp'
+maintainer 'Romi'
+maintainer_email 'romi.afr@bobobobo.com'
+license 'MIT'
+version '0.1.0'
+chef_version '>= 12.0'
+supports 'ubuntu'
+
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+issues_url 'https://github.com/romiafrizal/belajar-chef-hactiv8/issues'
+
+# The `source_url` points to the development repository for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+source_url 'https://github.com/romiafrizal/belajar-chef-hactiv8.git'
---
+++ recipes/php7.rb
@@ -0,0 +1,11 @@
+package "php" do
+  action :install
+end
+
+package "php-pear" do
+  action :install
+end
+
+package "php-mysql" do
+  action :install
+end
